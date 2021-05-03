dbae2275a00a5a2e7aec3667c4df95d97ddafaea
Kocsis Richárd <ricsik52@gmail.com>
2021-05-03 10:47:12 +0200

    tests

--- app/src/test/kotlin/hu/bme/kocsis/richard/animelist/ui/animelist/AnimeListActivityTest.kt
+++ app/src/test/kotlin/hu/bme/kocsis/richard/animelist/ui/animelist/AnimeListActivityTest.kt
@@ -1,35 +1,43 @@
 package hu.bme.kocsis.richard.animelist.ui.animelist
 
-import hu.bme.kocsis.richard.animelist.BuildConfig
-import org.junit.After
+import android.R
+import android.view.View
+import androidx.test.core.app.ActivityScenario.launch
+import junit.framework.Assert.assertNotNull
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
 import org.junit.runner.RunWith
+import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
+import org.robolectric.android.controller.ActivityController
 
 
+@RunWith(RobolectricTestRunner::class)
+//@Config(constants = BuildConfig::class)
 class AnimeListActivityTest {
+    private lateinit var animeListActivity: AnimeListActivity
+    private lateinit var animeListController: ActivityController<AnimeListActivity>
 
     @Before
-    fun setUp() {
-    }
+    @Throws(Exception::class)
+    fun setup() {
+// Calls the lifecycle: create-start-postCreate-resume
+        animeListController = Robolectric.buildActivity(AnimeListActivity::class.java)
+        animeListActivity = animeListController.get()
 
-    @After
-    fun tearDown() {
-    }
 
-    @Test
-    fun onCreate() {
-    }
-
-    @Test
-    fun onCreateOptionsMenu() {
     }
 
     @Test
-    fun onOptionsItemSelected() {
+    @Throws(java.lang.Exception::class)
+    fun shouldHaveAnimeListFragment() {
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testDetailsButtonIsDisplayed() {
+
+
     }
 }
---
+++ app/src/test/kotlin/hu/bme/kocsis/richard/animelist/ui/details/DetailsActivityTest.kt
@@ -0,0 +1,17 @@
+package hu.bme.kocsis.richard.animelist.ui.details
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class DetailsActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}

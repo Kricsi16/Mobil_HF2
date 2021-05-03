993b634317f621d8d3f9e4fe1520aa30c0150d87
Kocsis Richárd <ricsik52@gmail.com>
2021-05-03 09:34:42 +0200

    testfavourites

---
+++ app/src/test/kotlin/hu/bme/kocsis/richard/animelist/ui/favourites/FavouritesActivityTest.kt
@@ -0,0 +1,17 @@
+package hu.bme.kocsis.richard.animelist.ui.favourites
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class FavouritesActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
